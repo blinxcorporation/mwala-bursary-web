78023eda177db70f4bcad0be86fb0055340fd256
Benson <bensonmakau2000@gmail.com>
2022-06-06 15:03:58 +0300

    added footer

--- includes/navbar.php
+++ includes/navbar.php
@@ -13,7 +13,7 @@
         <a class="nav-link" href="#">Bursary Tracking(status)</a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="#">Updates</a>
+        <a class="nav-link" href="#">Updates(<span>0</span>)</a>
       </li>
       <li class="nav-item dropdown active">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
